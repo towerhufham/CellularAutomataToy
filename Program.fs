100add609ea588e36f401d2c6951cf03e6e9c688
towerhufham <32599109+towerhufham@users.noreply.github.com>
2025-11-20 21:21:11 -0500

    Basic grid

--- Program.fs
+++ Program.fs
@@ -13,31 +13,70 @@ module Main =
   let view () =
     Component(fun ctx ->
       let state = ctx.useState 0
-      DockPanel.create [
-        DockPanel.children [
-          Button.create [
-            Button.dock Dock.Bottom
-            Button.onClick (fun _ -> state.Set(state.Current - 1))
-            Button.content "-"
-            Button.horizontalAlignment HorizontalAlignment.Stretch
-            Button.horizontalContentAlignment HorizontalAlignment.Center
-          ]
-          Button.create [
-            Button.dock Dock.Bottom
-            Button.onClick (fun _ -> state.Set(state.Current + 1))
-            Button.content "+"
-            Button.horizontalAlignment HorizontalAlignment.Stretch
-            Button.horizontalContentAlignment HorizontalAlignment.Center
+      Grid.create [
+        Grid.columnDefinitions [
+          ColumnDefinition (GridLength 100)
+          ColumnDefinition (GridLength 100)
+        ]
+        Grid.rowDefinitions [
+          RowDefinition (GridLength 100)
+          RowDefinition (GridLength 100)
+        ]
+        Grid.children [
+          TextBlock.create [
+            Grid.column 0
+            Grid.row 0
+            TextBlock.text "1"
           ]
           TextBlock.create [
-            TextBlock.dock Dock.Top
-            TextBlock.fontSize 48.0
-            TextBlock.verticalAlignment VerticalAlignment.Center
-            TextBlock.horizontalAlignment HorizontalAlignment.Center
-            TextBlock.text (string state.Current)
+            Grid.column 1
+            Grid.row 0
+            TextBlock.text "2"
+          ]
+          TextBlock.create [
+            Grid.column 0
+            Grid.row 1
+            TextBlock.text "3"
+          ]
+          TextBlock.create [
+            Grid.column 1
+            Grid.row 1
+            TextBlock.text "4"
           ]
         ]
       ]
+      // DockPanel.create [
+      //   DockPanel.children [
+      //     Button.create [
+      //       Button.dock Dock.Bottom
+      //       Button.onClick (fun _ -> state.Set(state.Current - 1))
+      //       Button.content "-"
+      //       Button.horizontalAlignment HorizontalAlignment.Stretch
+      //       Button.horizontalContentAlignment HorizontalAlignment.Center
+      //     ]
+      //     Button.create [
+      //       Button.dock Dock.Bottom
+      //       Button.onClick (fun _ -> state.Set(state.Current + 1))
+      //       Button.content "+"
+      //       Button.horizontalAlignment HorizontalAlignment.Stretch
+      //       Button.horizontalContentAlignment HorizontalAlignment.Center
+      //     ]
+      //     TextBlock.create [
+      //       TextBlock.dock Dock.Top
+      //       TextBlock.fontSize 48.0
+      //       TextBlock.verticalAlignment VerticalAlignment.Center
+      //       TextBlock.horizontalAlignment HorizontalAlignment.Center
+      //       TextBlock.text (string state.Current)
+      //     ]
+      //     TextBlock.create [
+      //       TextBlock.dock Dock.Top
+      //       TextBlock.fontSize 48.0
+      //       TextBlock.verticalAlignment VerticalAlignment.Center
+      //       TextBlock.horizontalAlignment HorizontalAlignment.Center
+      //       TextBlock.text "Hi :3"
+      //     ]
+      //   ]
+      // ]
     )
 
 type MainWindow() =
